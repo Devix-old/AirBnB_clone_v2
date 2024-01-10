9e2e5ae66b9c3d18f07678cfab66d63ee778c9d4
Devix-old <mohamed.elgharef@gmail.com>
2024-01-10 22:09:00 +0100

    git add .!

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,8 @@
 
 apt-get update && \
 apt-get install -y nginx && \
-mkdir -p -m=755 /data/web_static/{releases/test,shared} || exit 0
+mkdir -p /data/web_static/{releases/test,shared} || exit 0
+chmod 755 /data/web_static/releases /data/web_static/shared
 echo 'Hello from nginx server' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
