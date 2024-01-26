d8935216ade13556572db440d1d02fc7e5a34df3
Devix-old <mohamed.elgharef@gmail.com>
2024-01-26 12:55:55 +0100

    git add .!

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+"""
+Flask Web App: Two routes displaying greetings on 0.0.0.0:5000.
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Route: /  Returns: str: Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Route: /hbnb  Returns: str: HBNB"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+"""
+Flask Web App: Three routes displaying greetings on 0.0.0.0:5000.
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Route: /  Returns: str: Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Route: /hbnb  Returns: str: HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """Route: /c/<text>  Returns: str: C followed by the value of text"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
