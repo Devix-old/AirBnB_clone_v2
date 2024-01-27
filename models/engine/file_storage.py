1d3d859607a3592ad1b0745a4b67d4a326af13a0
Devix-old <mohamed.elgharef@gmail.com>
2024-01-27 11:37:13 +0100

    task 7 update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,8 @@ class FileStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls:
-            return {key: val for key, val in FileStorage.__objects.items() if val.__class__ == cls}
+            return {key: val for key, val in
+                    FileStorage.__objects.items() if val.__class__ == cls}
         return FileStorage.__objects
 
     def new(self, obj):
--- models/state.py
+++ models/state.py
@@ -3,14 +3,17 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.city import City
+
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    
+
     # For DBStorage
-    cities = relationship('City', backref='state', cascade='all, delete-orphan')
+    cities = relationship('City', backref='state',
+                          cascade='all, delete-orphan')
 
     # For FileStorage
     @property
@@ -18,7 +21,7 @@ class State(BaseModel, Base):
         """ Getter attribute for cities in FileStorage """
         from models import storage
         city_list = []
-        for city_id, city in storage.all('City').items():
+        for city_id, city in storage.all(City).items():
             if city.state_id == self.id:
                 city_list.append(city)
         return city_list
