bc60f989714e41cfaeeec9f4e5ade3f16d554052
Devix-old <mohamed.elgharef@gmail.com>
2024-01-27 23:30:03 +0100

    task 9

---
+++ web_flask/9-states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application.
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Remove the current SQLAlchemy Session."""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states_list():
+    """Display a HTML page with a list of all State objects."""
+    states = storage.all(State).values()
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_cities(id):
+    """Display a HTML page with information about a specific State."""
+    state = storage.get(State, id)
+    if state:
+        return render_template('9-states.html', state=state)
+    return render_template('9-states.html', not_found=True)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
